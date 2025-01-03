905c6d40ca5c281c2a492c627f84a96b34c1d862
Agustín Concollato <agustinconcollato@gmail.com>
2025-01-03 00:18:31 -0300

    agregar método save a la clase Address

--- addressService.js
+++ addressService.js
@@ -1,17 +1,40 @@
 import { urlAddress } from "./api"
 
 export class Address {
-    constructor(parameters) { }
+    constructor(userId) {
+        this.userId = userId
+    }
 
     async get(userId) {
         try {
-            const response = await fetch(`${urlAddress}/${userId}`)
+            const response = await fetch(`${urlAddress}/${this.userId}`)
 
             if (!response.ok) {
                 throw new Error('Error al obtener las direcciones')
             }
 
             return await response.json()
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async save(address) {
+        try {
+            const response = await fetch(`${urlAddress}/${this.userId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ code: address.code })
+            })
+
+            if (!response.ok) {
+                throw new Error('Error al guardar la dirección')
+            }
+
+            return await response.json()
+
         } catch (error) {
             throw new Error(error)
         }
